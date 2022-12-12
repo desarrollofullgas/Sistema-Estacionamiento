457e19ba44bf2917f80187063602f11ee43327b3
Quen97 <Antonio1308_97@outlook.com>
2022-12-12 10:08:59 -0600

    policy and terms

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,16 @@
+<x-guest-layout>
+    <div class="row justify-content-center pt-4">
+        <div class="col-6">
+            <div>
+                <p align="center"><img src="{{ asset('img/logo/FullGas_rojo2.jpeg') }}" style="width: 250px !important"
+                    alt="FullGas"></p>
+            </div>
+
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    {!! $policy !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
